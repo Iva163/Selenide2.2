12a93c4b4157b3c4b2c60b276b98888fdb73cdfe
Ivan Borisov <ivan_borisov_88@vk.com>
2023-04-11 17:21:17 +0400

    change test

--- src/test/java/ru/netology/web/AppCardDeliveryTest.java
+++ src/test/java/ru/netology/web/AppCardDeliveryTest.java
@@ -2,62 +2,71 @@ package ru.netology.web;
 
 
 import com.codeborne.selenide.Condition;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 
-import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.exactText;
 
 
 public class AppCardDeliveryTest {
 
-
-    String date;
-
-    @BeforeEach
-    public void setup() {
-        Date dt = new Date();
-        Calendar c = Calendar.getInstance();
-        c.setTime(dt);
-        c.add(Calendar.DATE, 4);
-        dt = c.getTime();
-        date = new SimpleDateFormat("dd.MM.yyyy").format(dt);
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
+
 
     @Test
     void shouldTestWithInput() {
-
+        String planningDate = generateDate(4);
         open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Самара");
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
-        $("[data-test-id=date] input").setValue(date);
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Борисов Иван");
         $("[data-test-id=phone] input").setValue("+79012345678");
         $("[data-test-id=agreement]").click();
         $x("//button//span[contains(text(),'Забронировать')]").click();
         $x("//div[contains(text(),'Успешно!')]").shouldHave(Condition.visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
 
     }
 
     @Test
     void shouldTestWithSelect() {
+        String planningDate = generateDate(7);
+        LocalDate planDate = LocalDate.now().plusDays(7);
+        String day = String.valueOf(planDate.getDayOfMonth());
+        LocalDate defaultDate = LocalDate.now().plusDays(3);
 
         open("http://localhost:9999");
+
         $("[data-test-id=city] input").setValue("Са");
         $x("//span[contains(text(),'Самара')]").click();
+
         $("[data-test-id=date] input").click();
-        $("td.calendar__day_state_current").click();
+        if (planDate.getMonthValue() > defaultDate.getMonthValue() | planDate.getYear() > defaultDate.getYear()) {
+            $(".calendar__arrow_direction_right[data-step='1']").click();
+        }
+        $$("td.calendar__day").find(exactText(day)).click();
+
         $("[data-test-id=name] input").setValue("Борисов Иван");
         $("[data-test-id=phone] input").setValue("+79012345678");
         $("[data-test-id=agreement]").click();
         $x("//button//span[contains(text(),'Забронировать')]").click();
+
         $x("//div[contains(text(),'Успешно!')]").shouldHave(Condition.visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
 
     }
 
